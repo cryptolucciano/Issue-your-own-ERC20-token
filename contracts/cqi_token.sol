e13f2e76f899aea068dc16dc4132ac82db21aeb2
cryptolucciano <cryptolucciano@gmail.com>
2018-08-30 23:06:26 -1000

    Adapted the contract code for CQI

--- contracts/cqi_token.sol
+++ contracts/cqi_token.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.4.18;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'Coquí' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
+// Deployed to : 0xA12e7ab64d01521B0F1617E63caaD9E4f3F37a0A
+// Symbol      : CQI
+// Name        : Coquí Token
 // Total supply: 100000000
 // Decimals    : 18
 //
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract CoquiToken is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -112,13 +112,13 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // ------------------------------------------------------------------------
     // Constructor
     // ------------------------------------------------------------------------
-    function FucksToken() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
+    function CoquiToken() public {
+        symbol = "CQI";
+        name = "Coquí Token";
         decimals = 18;
         _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        balances[0xA12e7ab64d01521B0F1617E63caaD9E4f3F37a0A] = _totalSupply;
+        Transfer(address(0), 0xA12e7ab64d01521B0F1617E63caaD9E4f3F37a0A, _totalSupply);
     }
 
 
